d96c0cdff932fb2ded492de7e698622f8c46e0e6
Nelson Barrantes Artavia <nelson.atle@gmail.com>
2022-02-22 21:07:02 -0600

    new version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b444891081f7bd946c064e7a10c2322a",
-"index.html": "2628874f367b967586d6af9ba9df5003",
-"/": "2628874f367b967586d6af9ba9df5003",
-"main.dart.js": "41ff7a80f5ccee01bc453378234b8b1c",
+"index.html": "c520e9dfcb10a1a9932115a3f9820a29",
+"/": "c520e9dfcb10a1a9932115a3f9820a29",
+"main.dart.js": "e5128e77e3f7dd0f78c8b7092999321c",
 "favicon.png": "0fe38434e2598b61d489824b6e69896b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "58775449cd0a8712810bcd19269d8803",
 "assets/AssetManifest.json": "bf3bae37e8f645e2d4a633a5bd2abe6c",
-"assets/NOTICES": "6c984599dc224e662e8f48bc5c5bed2b",
+"assets/NOTICES": "2f45eac1662a85e7d2903e266ffc76e9",
 "assets/FontManifest.json": "2fe33702e0094d596583a78378983811",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
@@ -25,11 +25,7 @@ const RESOURCES = {
 "assets/assets/images/prep_item.png": "d3064bae445f1d46b825e201d6319463",
 "assets/assets/images/logo_app_bar.svg": "92c29ff4a7701459e1542371a4cc3aeb",
 "assets/assets/images/treatment_item.png": "ffcf3cb8c23a6131a32914e38f49e49b",
-"assets/assets/fonts/montserrat/Montserrat-SemiBold.ttf": "7ffeec2b4edb434d393875ffbe633c30",
-"canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
-"canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
-"canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
-"canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba"
+"assets/assets/fonts/montserrat/Montserrat-SemiBold.ttf": "7ffeec2b4edb434d393875ffbe633c30"
 };
 
 // The application shell files that are downloaded before a service worker can
