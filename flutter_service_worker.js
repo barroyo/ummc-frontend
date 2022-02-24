e8e604131b91fc97c76a90cef118fc3a1275842f
Nelson Barrantes Artavia <nelson.atle@gmail.com>
2022-02-24 13:26:20 -0600

    new version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b444891081f7bd946c064e7a10c2322a",
-"index.html": "c520e9dfcb10a1a9932115a3f9820a29",
-"/": "c520e9dfcb10a1a9932115a3f9820a29",
-"main.dart.js": "e5128e77e3f7dd0f78c8b7092999321c",
+"index.html": "fb3a06f42b76d39f9f1e8038fc78562b",
+"/": "fb3a06f42b76d39f9f1e8038fc78562b",
+"main.dart.js": "fef2cf917f7f7faa8eede156734a3cf7",
 "favicon.png": "0fe38434e2598b61d489824b6e69896b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -14,7 +14,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "58775449cd0a8712810bcd19269d8803",
 "assets/AssetManifest.json": "bf3bae37e8f645e2d4a633a5bd2abe6c",
-"assets/NOTICES": "2f45eac1662a85e7d2903e266ffc76e9",
+"assets/NOTICES": "fc0ffcf13edbfde3e2b7511f07b2b6ee",
 "assets/FontManifest.json": "2fe33702e0094d596583a78378983811",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
