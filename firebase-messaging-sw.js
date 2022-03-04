73eb9846458388b359820c831a93f0658cbc8cdc
Nelson Barrantes Artavia <nelson.atle@gmail.com>
2022-03-04 16:10:46 -0600

    new version

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+   /*Update with yours config*/
+  const firebaseConfig = {
+    apiKey: "AIzaSyA282vx5ZhdZwPtxhKSzCNfx5Zi5LJXGfs",
+    authDomain: "ummccareshiv.firebaseapp.com",
+    projectId: "ummccareshiv",
+    storageBucket: "ummccareshiv.appspot.com",
+    messagingSenderId: "722672967447",
+    appId: "1:722672967447:web:4e7e56a27b490fa5676152"
+ };
+  firebase.initializeApp(firebaseConfig);
+  const messaging = firebase.messaging();
+
+  /*messaging.onMessage((payload) => {
+  console.log('Message received. ', payload);*/
+  messaging.onBackgroundMessage(function(payload) {
+    console.log('Received background message ', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+    };
+
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
