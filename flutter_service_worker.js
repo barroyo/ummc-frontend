99bd58ac922be8840585d1a7650af749e8c0363c
Nelson Barrantes Artavia <nelson.atle@gmail.com>
2022-05-03 11:38:30 -0600

    new version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,10 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b444891081f7bd946c064e7a10c2322a",
-"index.html": "cb03f066a408449c05b2bfe8fbd0736a",
-"/": "cb03f066a408449c05b2bfe8fbd0736a",
+"index.html": "b4d1d2b6c91192465d38cd4542d3b819",
+"/": "b4d1d2b6c91192465d38cd4542d3b819",
 "firebase-messaging-sw.js": "1a0bbf6827596fdfc63a74ca0a50a1e4",
-"main.dart.js": "59835040122c3e02b60ed5a108e6a566",
+"main.dart.js": "b308f51758095a177239a2cd904d04bd",
 "favicon.png": "0fe38434e2598b61d489824b6e69896b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
