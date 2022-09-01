74de97e969c02ad8e0addcde94797d1c1ac27aac
Javier Gonzalez <nelson.atle@gmail.com>
2022-09-01 08:59:47 -0600

    new version

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+importScripts('functions.js');
 
    /*Update with yours config*/
   const firebaseConfig = {
@@ -13,27 +14,8 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
   firebase.initializeApp(firebaseConfig);
   const messaging = firebase.messaging();
 
-  /*messaging.onMessage((payload) => {
-  console.log('Message received. ', payload);*/
-  // messaging.onBackgroundMessage(function(payload) {
-  //   console.log('Received background message ', payload);
-
-  //   const notificationTitle = payload.notification.title;
-  //   const notificationOptions = {
-  //     body: payload.notification.body,
-  //   };
-
-  //   self.registration.showNotification(notificationTitle,
-  //     notificationOptions);
-  // });
-
-  messaging.onBackgroundMessage((payload) => {
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-      body: payload.notification.body,
-      icon: "icon.png"
-    };
-  
-    self.registration.showNotification(notificationTitle,
-      notificationOptions);
+  messaging.onBackgroundMessage((message) => {
+    reloadPage();
+    console.log("onBackgroundMessage", message);
   });
+  
